9ef88633511f59515809dbb09dca74f82dc79a8f
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-10-16 12:54:45 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -16,14 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-from django.urls import path, include, re_path
-from django.views.static import serve
 urlpatterns = [
     path('', include('blog_app.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
 
 # if settings.DEBUG:
