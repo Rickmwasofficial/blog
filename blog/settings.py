74ae36c971be45aff49a565002bdc71b08d6eadd
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-10-16 13:11:38 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static",
 ]
 STATIC_ROOT = '/opt/render/project/static_root'
 
